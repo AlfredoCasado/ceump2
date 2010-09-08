0599a4e23c945446dfa0030d103f00f8d1439547
casado.alfredo <casado.alfredo@1e037dc0-25c0-b5b7-663a-916ae1610b51>
2010-09-08 17:54:06 +0000

    estaba fatal el mensaje

--- trunk/EjemplosMPII/src/ejemplosmpii/Main.java
+++ trunk/EjemplosMPII/src/ejemplosmpii/Main.java
@@ -3,7 +3,7 @@ package ejemplosmpii;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Comoienza el cuso 2010-1011!");
+        System.out.println("Comienza el curso 2010-2011!");
     }
 
 }
