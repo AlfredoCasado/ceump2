bc2bc352b154f5f4644508a4ea7b549228013f9c
casado.alfredo <casado.alfredo@1e037dc0-25c0-b5b7-663a-916ae1610b51>
2010-09-14 13:29:18 +0000

    subidos test realizados en la clase del jueves día 10.

---
+++ EjemplosMPII/test/es/ceu/mpII/introduccion/ControlDeFlujoTest.java
@@ -0,0 +1,66 @@
+package es.ceu.mpII.introduccion;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class ControlDeFlujoTest {
+
+    @Test
+    public void pruebaIf() {
+        int numeroDeAlumnos = 7;
+
+        if (numeroDeAlumnos == 7) {
+            assertTrue(true);
+        } else {
+            fail("el if no funciona???");
+        }
+    }
+
+    @Test
+    public void pruebaWhile() {
+
+        int numeroDeAlumnos = 0;
+        while (numeroDeAlumnos < 7) {
+            numeroDeAlumnos++;
+        }
+
+        assertEquals(7, numeroDeAlumnos);
+    }
+
+    @Test
+    public void pruebaDoWhile() {
+        int numeroDeAlumnos = 7;
+        do {
+            numeroDeAlumnos--;
+        } while (numeroDeAlumnos > 0);
+
+        assertEquals(0, numeroDeAlumnos);
+    }
+
+    @Test
+    public void pruebaFor() {
+        int numeroDeAlumnos = 0;
+        for (int i = 0; i < 10; i++) {
+            numeroDeAlumnos++;
+        }
+
+        assertEquals(10, numeroDeAlumnos);
+    }
+
+    @Test
+    public void pruebaSwitch() {
+
+        switch(2) {
+            case 1: {
+                    fail("era 2!!");
+
+            }
+            case 2: assertTrue(true); break;
+            default: fail("era 2");
+        }
+
+    }
+
+}
