0f8bbd1860be446919a88f61ee86baca851fa190
casado.alfredo <casado.alfredo@1e037dc0-25c0-b5b7-663a-916ae1610b51>
2010-09-23 16:51:14 +0000

    Terminado ejercicio de trenes

--- EjemplosMPII/src/es/ceu/mpII/introduccion/ejerciciocargatrenes/Tren.java
+++ EjemplosMPII/src/es/ceu/mpII/introduccion/ejerciciocargatrenes/Tren.java
@@ -2,12 +2,24 @@ package es.ceu.mpII.introduccion.ejerciciocargatrenes;
 
 public class Tren {
 
+    private final int pesoMaximo;
+    private int cargaActual = 0;
+
+    public Tren(int pesoMaximo) {
+        this.pesoMaximo = pesoMaximo;
+    }
+
     boolean tieneEspacioDisponiblePara(Mercancia mercancia) {
-        throw new UnsupportedOperationException("Not yet implemented");
+        int pesoDeLaMercancia = mercancia.dameTuPeso();
+        return pesoDeLaMercancia + cargaActual <= pesoMaximo;
     }
 
     void guardar(Mercancia mercancia) {
-        throw new UnsupportedOperationException("Not yet implemented");
+        cargaActual+= mercancia.dameTuPeso();
+    }
+
+    int dameCargaActual() {
+        return cargaActual;
     }
 
 }
