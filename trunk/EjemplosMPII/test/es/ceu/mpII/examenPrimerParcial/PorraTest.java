33b70c219bfcbe353073c9d1616a3b38ba41bb10
casado.alfredo <casado.alfredo@1e037dc0-25c0-b5b7-663a-916ae1610b51>
2010-12-02 17:19:51 +0000

    añadido a la clase porra

--- trunk/EjemplosMPII/test/es/ceu/mpII/examenPrimerParcial/PorraTest.java
+++ trunk/EjemplosMPII/test/es/ceu/mpII/examenPrimerParcial/PorraTest.java
@@ -1,9 +1,16 @@
 package es.ceu.mpII.examenPrimerParcial;
 
+import java.util.List;
+import java.util.Set;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class PorraTest {
+
+    public static final int ADMITE_UNA_APUESTA_REPETIDA = 1;
+    public static final int NUMERO_DE_GANADORES_ESPERADO = 1;
+    public static final int ADMITE_DOS_APUESTAS_REPETIDA = 2;
+
 
     final Usuario julian = new Usuario("Julian");
     final Usuario alfredo = new Usuario("Alfredo");
@@ -13,15 +20,44 @@ public class PorraTest {
 
     @Test
     public void hacemosVariasPuestasYNosDevuelveElGanadorCuandoNosDicenElResultadoDelPartido() {
-
-        Porra porraBarsaMadrid = new Porra(barsaMadrid);
+        Porra porraBarsaMadrid = new Porra(barsaMadrid, ADMITE_UNA_APUESTA_REPETIDA);
 
         porraBarsaMadrid.nuevaApuesta(alfredo, resultadoOptimista);
         porraBarsaMadrid.nuevaApuesta(julian, elBarsaArrasa);
 
-        Usuario usuarioGanador = porraBarsaMadrid.resultadoDelPartido(new Resultado(5, 0));
+        Set<Usuario> ganadores = porraBarsaMadrid.resultadoDelPartido(new Resultado(5, 0));
 
-        assertEquals(julian, usuarioGanador);
+        assertEquals(NUMERO_DE_GANADORES_ESPERADO, ganadores.size());
+        for (Usuario usuario : ganadores) {
+            if (usuario.equals(julian)) {
+                return;
+            }
+        }
+        fail("julian debería ser el ganador");
+    }
+
+    @Test
+    public void siNoTenemosNingunAcertanteDevolvemosUnaListaVaciaDeGanadores() {
+        Porra porraBarsaMadrid = new Porra(barsaMadrid, ADMITE_UNA_APUESTA_REPETIDA);
+        Set<Usuario> ganadores = porraBarsaMadrid.resultadoDelPartido(new Resultado(5, 0));
+        assertTrue(ganadores.isEmpty());
+    }
+
+    @Test
+    public void laPorraSoloAdmiteLasApuestasRepetidasQueSeLeIndiquen() {
+        Porra porraBarsaMadrid = new Porra(barsaMadrid, ADMITE_UNA_APUESTA_REPETIDA);
+
+        assertTrue(porraBarsaMadrid.nuevaApuesta(alfredo, resultadoOptimista));
+        assertFalse(porraBarsaMadrid.nuevaApuesta(julian, resultadoOptimista));
+    }
+
+    @Test
+    public void laPorraSoloAdmiteLasApuestasRepetidasQueSeLeIndiquenConDosApuestas() {
+        Porra porraBarsaMadrid = new Porra(barsaMadrid, ADMITE_DOS_APUESTAS_REPETIDA);
+
+        assertTrue(porraBarsaMadrid.nuevaApuesta(alfredo, resultadoOptimista));
+        assertTrue(porraBarsaMadrid.nuevaApuesta(alfredo, resultadoOptimista));
+        assertFalse(porraBarsaMadrid.nuevaApuesta(julian, resultadoOptimista));
     }
 
 }
