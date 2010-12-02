112a4aac6a02e71411b81e07d1c135fc066e42f5
casado.alfredo <casado.alfredo@1e037dc0-25c0-b5b7-663a-916ae1610b51>
2010-12-02 17:19:51 +0000

    añadido a la clase porra

--- EjemplosMPII/src/es/ceu/mpII/examenPrimerParcial/Porra.java
+++ EjemplosMPII/src/es/ceu/mpII/examenPrimerParcial/Porra.java
@@ -6,22 +6,46 @@ import java.util.Set;
 public class Porra {
     private final Partido partido;
     private Set<Apuesta> apuestas = new HashSet();
+    private final int apuestasRepetidasQueAdmiteLaPorra;
 
-    public Porra(Partido partido) {
+    public Porra(Partido partido, int apuestasRepetidasQueAdmiteLaPorra) {
         this.partido = partido;
+        this.apuestasRepetidasQueAdmiteLaPorra = apuestasRepetidasQueAdmiteLaPorra;
     }
 
-    public void nuevaApuesta(Usuario usuario, Resultado resultado) {
+    public boolean nuevaApuesta(Usuario usuario, Resultado resultado) {
+        int apuestasRepetidas = contarResultadosRepetidos(resultado);
+
+        if (superaElMaximoDeApuestasPermitidas(apuestasRepetidas)) {
+            return false;
+        }
+
         apuestas.add(new Apuesta(usuario,resultado));
+        return true;
     }
 
-    public Usuario resultadoDelPartido(Resultado resultadoFinalDelPartido) {
+    private boolean superaElMaximoDeApuestasPermitidas(int apuestasRepetidas) {
+        return apuestasRepetidasQueAdmiteLaPorra <= apuestasRepetidas;
+    }
+
+    private int contarResultadosRepetidos(Resultado resultado) {
+        int apuestasRepetidas = 0;
         for (Apuesta apuesta : apuestas) {
-            if (apuesta.tieneElResultado(resultadoFinalDelPartido)) {
-                return apuesta.usuario();
+            if (apuesta.tieneElResultado(resultado)) {
+                apuestasRepetidas++;
             }
         }
-        return null;
+        return apuestasRepetidas;
+    }
+
+    public Set<Usuario> resultadoDelPartido(Resultado resultadoFinalDelPartido) {
+        Set<Usuario> ganadores = new HashSet();
+        for (Apuesta apuesta : apuestas) {
+            if (apuesta.tieneElResultado(resultadoFinalDelPartido)) {
+                ganadores.add(apuesta.usuario());
+            }
+        }
+        return ganadores;
     }
 
 }
