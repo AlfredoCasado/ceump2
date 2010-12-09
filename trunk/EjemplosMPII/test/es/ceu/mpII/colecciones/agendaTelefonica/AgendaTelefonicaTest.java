5f0bafc96e0ddaf2536b1f104a98fbc9684282b9
casado.alfredo <casado.alfredo@1e037dc0-25c0-b5b7-663a-916ae1610b51>
2010-12-09 18:07:46 +0000

    añadido a la clase porra

---
+++ trunk/EjemplosMPII/test/es/ceu/mpII/colecciones/agendaTelefonica/AgendaTelefonicaTest.java
@@ -0,0 +1,42 @@
+package es.ceu.mpII.colecciones.agendaTelefonica;
+
+import es.ceu.mpII.colecciones.agendaTelefonica.AgendaTelefonica;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class AgendaTelefonicaTest {
+
+    @Test
+    public void puedoAnadirUnElementoYLuegoConsultarlo() {
+        AgendaTelefonica agenda = new AgendaTelefonica();
+        agenda.nuevoContacto("alfredo", "12345");
+
+        assertEquals("12345", agenda.telefonoDe("alfredo"));
+    }
+    
+    @Test
+    public void podemosEliminarUnElementoDeLaAgenda() {
+        AgendaTelefonica agenda = new AgendaTelefonica();
+        agenda.nuevoContacto("alfredo", "12345");
+        agenda.eliminarContacto("alfredo");
+      
+        assertFalse(agenda.tengoElTelefonoDe("alfredo"));
+    }
+
+    @Test
+    public void puedoPintarLaAgenda() {
+        AgendaTelefonica agenda = new AgendaTelefonica();
+        agenda.nuevoContacto("alfredo", "12345");
+        agenda.nuevoContacto("pedro", "1234533");
+        agenda.nuevoContacto("paco", "3434334");
+
+        agenda.pintarAgendaRecorrerConEntry();
+    }
+
+
+
+}
