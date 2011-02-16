9d1c1c584e131eef989cf5110527ccd00c7ecfc6
casado.alfredo <casado.alfredo@1e037dc0-25c0-b5b7-663a-916ae1610b51>
2011-02-16 19:23:30 +0000

    subidos ejemplos vistos en clase de jdbc

--- EjemplosMPII/test/es/ceu/mpII/polimorfismo/enviodenotas/SistemaEnvioNotificacionesTest.java
+++ EjemplosMPII/test/es/ceu/mpII/polimorfismo/enviodenotas/SistemaEnvioNotificacionesTest.java
@@ -4,6 +4,7 @@ import es.ceu.mpII.polimorfismo.enviodenotas.notificadores.Notificador;
 import es.ceu.mpII.polimorfismo.enviodenotas.notificadores.NotificadorMail;
 import es.ceu.mpII.polimorfismo.enviodenotas.notificadores.NotificadorSms;
 import es.ceu.mpII.polimorfismo.enviodenotas.repositorio.RepositorioAlumnosEnMemoria;
+import es.ceu.mpII.polimorfismo.enviodenotas.repositorio.RepositorioDeAlumnosEnBaseDeDatos;
 import org.junit.Test;
 
 public class SistemaEnvioNotificacionesTest {
@@ -17,6 +18,20 @@ public class SistemaEnvioNotificacionesTest {
 
         SistemaEnvioNotificaciones envioNotificaciones =
                 new SistemaEnvioNotificaciones(new RepositorioAlumnosEnMemoria(),
+                                               notificadores);
+
+        envioNotificaciones.enviarNotificaciones();
+    }
+    
+    @Test
+    public void verificarQueFuncionaElSistemaDeEnvioDeNotificacionesConUnRepositorioEnBaseDeDatos() {
+
+        Notificador[] notificadores = new Notificador[2];
+        notificadores[0] = new NotificadorMail();
+        notificadores[1] = new NotificadorSms();
+
+        SistemaEnvioNotificaciones envioNotificaciones =
+                new SistemaEnvioNotificaciones(new RepositorioDeAlumnosEnBaseDeDatos(),
                                                notificadores);
 
         envioNotificaciones.enviarNotificaciones();
