c1fb5efccae3cf1cb126aa4291e24f612d2ae3f0
casado.alfredo@gmail.com <casado.alfredo@gmail.com@1e037dc0-25c0-b5b7-663a-916ae1610b51>
2011-04-27 17:59:17 +0000

    ejercicio maquina expendedora

--- trunk/EjemplosMPII/test/es/ceu/mpII/kata/maquina/Moneda.java
+++ trunk/EjemplosMPII/test/es/ceu/mpII/kata/maquina/Moneda.java
@@ -2,7 +2,24 @@ package es.ceu.mpII.kata.maquina;
 
 enum Moneda {
 
-    CINCO, DIEZ, VEINTE, CICUENTA, UN_EURO, DOS_EUROS
+    CINCO(5),
+    DIEZ(10),
+    VEINTE(20),
+    CICUENTA(50),
+    UN_EURO(100),
+    DOS_EUROS(200);
+
+    private final int centimos;
+
+    Moneda(int centimos) {
+        this.centimos = centimos;
+    }
+
+    public int getCentimos() {
+        return centimos;
+    }
+
+
 
 }
 
