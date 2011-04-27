3fb6b4ff07cffda58563aa1453643f87b0b9f20d
casado.alfredo@gmail.com <casado.alfredo@gmail.com@1e037dc0-25c0-b5b7-663a-916ae1610b51>
2011-04-27 17:16:28 +0000

    ejercicio maquina expendedora

---
+++ EjemplosMPII/test/es/ceu/mpII/kata/maquina/MaquinaExpendedora.java
@@ -0,0 +1,11 @@
+package es.ceu.mpII.kata.maquina;
+
+import java.util.List;
+
+class MaquinaExpendedora {
+
+    List<Moneda> comprar(String string, int i) {
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+}
---
+++ EjemplosMPII/test/es/ceu/mpII/kata/maquina/Moneda.java
@@ -0,0 +1,9 @@
+package es.ceu.mpII.kata.maquina;
+
+enum Moneda {
+
+    CINCO, DIEZ, VEINTE, CICUENTA, UN_EURO, DOS_EUROS
+
+}
+
+
