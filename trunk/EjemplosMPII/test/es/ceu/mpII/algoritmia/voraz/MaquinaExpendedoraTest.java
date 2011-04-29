feac075fdb557cbd8e51bcb0fe011e14c257f725
casado.alfredo@gmail.com <casado.alfredo@gmail.com@1e037dc0-25c0-b5b7-663a-916ae1610b51>
2011-04-29 18:01:47 +0000

    algoritmos voraces

---
+++ trunk/EjemplosMPII/test/es/ceu/mpII/algoritmia/voraz/MaquinaExpendedoraTest.java
@@ -0,0 +1,32 @@
+
+package es.ceu.mpII.algoritmia.voraz;
+
+import java.util.List;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class MaquinaExpendedoraTest {
+
+    MaquinaExpendedora maquina = new MaquinaExpendedora();
+
+    
+    @Test
+    public void si_le_doy_el_dinero_exacto_devuelve_una_lista_vacia_de_monedas_de_cambio() {
+        List<Moneda> cambio = maquina.comprar("coca-cola", 60);
+        assertEquals(0, cambio.size());
+    }
+
+    @Test
+    public void si_el_cambio_es_de_140_me_tiene_que_devolver_euro_20c_y_20c() {
+        List<Moneda> cambio = maquina.comprar("coca-cola", 200);
+        assertEquals(3, cambio.size());
+        assertEquals(Moneda.UN_EURO, cambio.get(0));
+        assertEquals(Moneda.VEINTE, cambio.get(1));
+        assertEquals(Moneda.VEINTE, cambio.get(2));
+
+    }
+
+
+
+
+}
---
+++ trunk/EjemplosMPII/test/es/ceu/mpII/algoritmia/voraz/OrdenadorVoraz.java
@@ -0,0 +1,26 @@
+package es.ceu.mpII.algoritmia.voraz;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class OrdenadorVoraz {
+
+    List<Integer> ordenar(List<Integer> listaDesordenada) {
+        List<Integer> listaOrdenada = new ArrayList();
+        while(!listaDesordenada.isEmpty()) {
+            Integer mayor = mayorDe(listaDesordenada);
+            listaOrdenada.add(mayor);
+            listaDesordenada.remove(mayor);
+        }
+        return listaOrdenada;
+    }
+
+    private Integer mayorDe(List<Integer> listaDesordenada) {
+        Integer mayor = Integer.MIN_VALUE;
+        for (Integer numero : listaDesordenada) {
+            if (numero > mayor) mayor = numero;
+        }
+        return mayor;
+    }
+
+}
