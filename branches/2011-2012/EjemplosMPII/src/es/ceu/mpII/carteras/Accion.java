3aaa9590cb50e3113c2d72ef6329a67724020055
casado.alfredo@gmail.com <casado.alfredo@gmail.com@1e037dc0-25c0-b5b7-663a-916ae1610b51>
2011-12-07 19:01:43 +0000

    ejercicio carteras - parte 2 actualizacion

--- branches/2011-2012/EjemplosMPII/src/es/ceu/mpII/carteras/Accion.java
+++ branches/2011-2012/EjemplosMPII/src/es/ceu/mpII/carteras/Accion.java
@@ -3,15 +3,21 @@ package es.ceu.mpII.carteras;
 class Accion {
 
     private int cotizacion;
+    private final ActualizadorCotizaciones actualizador;
 
-    public Accion(int cotizacion) {
+    public Accion(int cotizacion, ActualizadorCotizaciones actualizador) {
         this.cotizacion = cotizacion;
+        this.actualizador = actualizador;
     }
 
     
 
     long valorPara(int unidades) {
         return cotizacion*unidades;
+    }
+
+    void actualizar() {
+        cotizacion = actualizador.cotizacionActualDe(this);
     }
 
 }
