00135ffff334d96a4f6afa7b758e6a13a0997a66
casado.alfredo@gmail.com <casado.alfredo@gmail.com@1e037dc0-25c0-b5b7-663a-916ae1610b51>
2011-12-07 19:22:05 +0000

    ejercicio carteras - parte 3 actualizacion todas las carteras

--- EjemplosMPII/src/es/ceu/mpII/carteras/Eurobits.java
+++ EjemplosMPII/src/es/ceu/mpII/carteras/Eurobits.java
@@ -1,5 +1,8 @@
 package es.ceu.mpII.carteras;
 
+import es.ceu.mpII.carteras.Accion;
+import es.ceu.mpII.carteras.ActualizadorMovimientos;
+
 class Eurobits implements ActualizadorMovimientos {
 
     public int movimientosDe(Accion accion) {
