5e0c9f5b1517008f659444023ed3e08ca9a5cee4
casado.alfredo@gmail.com <casado.alfredo@gmail.com@1e037dc0-25c0-b5b7-663a-916ae1610b51>
2012-02-10 17:20:09 +0000

    primer ejercicio examen

---
+++ EjemplosMPII/test/es/ceu/mpII/primerParcial/grupos/GrupoTest.java
@@ -0,0 +1,21 @@
+package es.ceu.mpII.primerParcial.grupos;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class GrupoTest {
+
+    
+    @Test
+    public void testDamePermisos() {
+        
+        
+
+
+    }
+
+}
