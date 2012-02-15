c0ba835fbfb8077d4ad1fa962758a62cf522ecbe
casado.alfredo@gmail.com <casado.alfredo@gmail.com@1e037dc0-25c0-b5b7-663a-916ae1610b51>
2012-02-15 19:13:28 +0000

    primer ejercicio examen

--- branches/2011-2012/EjemplosMPII/test/es/ceu/mpII/primerParcial/grupos/GrupoTest.java
+++ branches/2011-2012/EjemplosMPII/test/es/ceu/mpII/primerParcial/grupos/GrupoTest.java
@@ -1,21 +1,41 @@
 package es.ceu.mpII.primerParcial.grupos;
 
-import org.junit.After;
-import org.junit.AfterClass;
-import org.junit.Before;
-import org.junit.BeforeClass;
+import java.util.HashSet;
+import java.util.Set;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class GrupoTest {
 
-    
-    @Test
-    public void testDamePermisos() {
-        
-        
 
+    private Set<Permiso> permisos(String...nombres) {
+        Set<Permiso> permisos = new HashSet();
 
+        for (String permiso : nombres) {
+            permisos.add(new Permiso(permiso));
+        }
+
+        return permisos;
     }
+
+    @Test
+    public void testDamePermisosConUnSoloGrupo() {
+        Grupo all = new Grupo("all", permisos("A"), new HashSet());
+
+        assertEquals(permisos("A"), all.damePermisos());
+    }
+
+    @Test
+    public void testDamePermisosConUnGrupoYUnUsuario() {
+        Set<ElementoConPermisos> elmentosQueContieneAll = new HashSet();
+        Usuario alfredo = new Usuario("alfredo", permisos("A","B","D"));
+        elmentosQueContieneAll.add(alfredo);
+
+        Grupo all = new Grupo("all", permisos("A","B","C"), elmentosQueContieneAll);
+
+        assertEquals(permisos("A","B","C","D"), alfredo.damePermisos());
+    }
+
+
 
 }
